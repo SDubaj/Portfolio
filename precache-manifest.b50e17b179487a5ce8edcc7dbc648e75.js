32b0ce622934413c2cf9f4b27db92d90f9a2014f
SDubaj <dubajsebastian96@gmail.com>
2020-05-28 22:37:11 +0200

    Updates

---
+++ precache-manifest.b50e17b179487a5ce8edcc7dbc648e75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ef2f1eb68b1be2e3004f574c055f3b3",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "cc76d515a0b07dd96425",
+    "url": "/Portfolio/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "5ea54a7d35c90df7e0f0",
+    "url": "/Portfolio/static/css/main.8a9c55a9.chunk.css"
+  },
+  {
+    "revision": "cc76d515a0b07dd96425",
+    "url": "/Portfolio/static/js/2.8e13c354.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Portfolio/static/js/2.8e13c354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea54a7d35c90df7e0f0",
+    "url": "/Portfolio/static/js/main.093bb579.chunk.js"
+  },
+  {
+    "revision": "654ea12aa83236a26bec",
+    "url": "/Portfolio/static/js/runtime-main.e1c49509.js"
+  }
+]);
